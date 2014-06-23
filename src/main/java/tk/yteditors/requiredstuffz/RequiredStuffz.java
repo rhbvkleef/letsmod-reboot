b5fb747b0b1849ca10d15e5c2608eafefba95cfb
rhbvkleef <rhbvkleef@gmail.com>
2014-06-23 09:59:17 +0200

    - Changed blockOven texturing mechanics and improved its tile-entity  - Added itemUnbakedPizza  - Added itemBakedPizza

--- src/main/java/tk/yteditors/requiredstuffz/RequiredStuffz.java
+++ src/main/java/tk/yteditors/requiredstuffz/RequiredStuffz.java
@@ -1,7 +1,11 @@
 package tk.yteditors.requiredstuffz;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import tk.yteditors.requiredstuffz.block.BlockOven;
+import tk.yteditors.requiredstuffz.item.ItemBakedPizza;
+import tk.yteditors.requiredstuffz.item.ItemUnbakedPizza;
+import tk.yteditors.requiredstuffz.reference.BlockNames;
 import tk.yteditors.requiredstuffz.reference.ModInfo;
 import tk.yteditors.requiredstuffz.tileentity.TileEntityOven;
 import tk.yteditors.requiredstuffz.util.RegisterHelper;
@@ -14,12 +18,40 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @Mod(modid=ModInfo.modId, name="Required Stuffz", version="1.7.2-A-1.0")
 public class RequiredStuffz {
 	
-	public static Block oven;
+	//@SidedProxy(clientSide = "tk.yteditors.requiredstuffz.proxy.CientProxy", serverSide = "tk.yteditors.requiredstuffz.proxy.ServerProxy")
+	//public static ServerProxy proxy;
+	
+	public static Block blockOvenOffEmpty;
+	public static Block blockOvenOffFilled;
+	public static Block blockOvenBurningEmpty;
+	public static Block blockOvenBurningFilled;
+	
+	public static Item itemUnbakedPizza;
+	public static Item itemBakedPizza;
 	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent e){
-		RegisterHelper.registerBlock(new BlockOven());
+		
+		blockOvenOffEmpty = new BlockOven(false, false).setBlockName(BlockNames.blockOven + "OffEmpty");;
+		blockOvenOffFilled = new BlockOven(false, true).setBlockName(BlockNames.blockOven + "OffFilled");;
+		blockOvenBurningEmpty = new BlockOven(true, false).setBlockName(BlockNames.blockOven + "BurningEmpty");;
+		blockOvenBurningFilled = new BlockOven(true, true).setBlockName(BlockNames.blockOven + "BurningFilled");
+		
+		RegisterHelper.registerBlock(blockOvenOffEmpty);
+		RegisterHelper.registerBlock(blockOvenOffFilled);
+		RegisterHelper.registerBlock(blockOvenBurningEmpty);
+		RegisterHelper.registerBlock(blockOvenBurningFilled);
+		
+		itemUnbakedPizza = new ItemUnbakedPizza();
+		itemBakedPizza = new ItemBakedPizza();
+		
+		RegisterHelper.registerItem(itemUnbakedPizza);
+		RegisterHelper.registerItem(itemBakedPizza);
+		
 		GameRegistry.registerTileEntity(TileEntityOven.class, "TileEntityOven");
+		
+		
+		//proxy.registerRender();
 	}
 	
 	@Mod.EventHandler
