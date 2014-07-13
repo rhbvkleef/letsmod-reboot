dcc071a996afb43f667f3ac75a19659628e2db71
rhbvkleef <rhbvkleef@gmail.com>
2014-07-13 10:51:12 +0200

    Messed around with intelliJ

--- src/main/java/tk/yteditors/requiredstuffz/util/RegisterHelper.java
+++ src/main/java/tk/yteditors/requiredstuffz/util/RegisterHelper.java
@@ -9,7 +9,7 @@ public class RegisterHelper {
 	public static void registerBlock(Block block) {
 		GameRegistry.registerBlock(block, ModInfo.MOD_ID + "_" + block.getUnlocalizedName().substring(5));
 	}
-	
+
 	public static void registerItem(Item item) {
 		GameRegistry.registerItem(item, ModInfo.MOD_ID + "_" + item.getUnlocalizedName().substring(5));
 	}
